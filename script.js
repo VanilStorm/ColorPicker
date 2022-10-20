14344d73d2033324cab357d1edd73ed40d74f147
VanilStorm <vanil.work@mail.ru>
2022-10-20 16:47:41 +0300

    Added copy and free from reload features

--- script.js
+++ script.js
@@ -14,18 +14,29 @@ document.addEventListener('click', e => {
         const node = e.target.tagName.toLowerCase() === 'i' ? e.target : e.target.children[0];
         node.classList.toggle('fa-lock-open');
         node.classList.toggle('fa-lock');
+    } else if (type === 'copy') {
+        copyHexColor(e.target.textContent);
     }
 })
 
-function setColors() {
-    cols.forEach(col => {
+function setColors(isInitial) {
+    const colors = isInitial ? getColorsFromHash() : [];
+
+    cols.forEach((col, index) => {
         const isLocked = col.querySelector('i').classList.contains('fa-lock');
         const text = col.querySelector('h2');
         const button = col.querySelector('button');
-        const color = getRandomColor();
+
 
         if (isLocked) {
-           return;
+            colors.push(text.textContent)
+            return;
+        }
+
+        const color = isInitial ? colors[index] : getRandomColor();
+
+        if (!isInitial) {
+            colors.push(color);
         }
 
         text.textContent = color;
@@ -33,6 +44,12 @@ function setColors() {
         setTextColor(text, color);
         setTextColor(button, color);
     })
+
+    updateColorsHash(colors);
+}
+
+function copyHexColor(text) {
+    return navigator.clipboard.writeText(text);
 }
 
 function getRandomColor() {
@@ -51,6 +68,17 @@ function setTextColor(elem, color) {
     elem.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
-setColors();
+function updateColorsHash(colors = []) {
+    document.location.hash = colors.map(item => item.substring(1)).join('-');
+}
+
+function getColorsFromHash() {
+    if (document.location.hash.length > 1) {
+        return document.location.hash.substring(1).split('-').map(color => "#" + color)
+    }
+    return [];
+}
+
+setColors(true);
 
 // alert('Press the Space bar to refresh the colors')
