e6029e9ebaf71b98a412dd19fae4d4296b6a6b1f
VanilStorm <vanil.work@mail.ru>
2022-10-20 16:08:02 +0300

    Added lock switcher

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-const cols = document.querySelectorAll('.col')
+const cols = document.querySelectorAll('.col');
 
-function setColors () {
+document.addEventListener('keydown', e => {
+    e.preventDefault()
+    if (e.code.toLowerCase() === 'space') {
+        setColors();
+    }
+})
+
+document.addEventListener('click', e => {
+    const type = e.target.dataset.type;
+
+    if (type === 'lock') {
+        const node = e.target.tagName.toLowerCase() === 'i' ? e.target : e.target.children[0];
+        node.classList.toggle('fa-lock-open');
+        node.classList.toggle('fa-lock');
+    }
+})
+
+function setColors() {
     cols.forEach(col => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock');
         const text = col.querySelector('h2');
+        const button = col.querySelector('button');
         const color = getRandomColor();
 
+        if (isLocked) {
+           return;
+        }
+
         text.textContent = color;
-        col.style.background = color;
+        col.style.background = color; //chroma.random()
+        setTextColor(text, color);
+        setTextColor(button, color);
     })
 }
 
@@ -14,11 +39,18 @@ function getRandomColor() {
     const hex = '0123456789ABCDEF';
     let color = '';
 
-    for(let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * hex.length)]
     }
 
     return '#' + color;
 }
 
+function setTextColor(elem, color) {
+    const luminance = chroma(color).luminance();
+    elem.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
 setColors();
+
+// alert('Press the Space bar to refresh the colors')
